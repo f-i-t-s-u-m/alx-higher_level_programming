fa624b30b397fd96c6ae7a1b085c3ba33894d747
f-i-t-s-u-m <fibabirr@gmail.com>
2021-11-16 15:25:40 +0300

    update .

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+def get_last():
+    if(number < 0):
+        return number % 10 - 10
+    return number % 10
+def comval():
+    if get_last() < 6 and get_last != 0:
+        return 'and is less than 6 and not 0'
+    elif get_last() < 6 and get_last == 0:
+        return 'and is less than 6 and is 0'
+    elif get_last() > 5:
+        return 'and is greater than 5'
+print("Last digit of {} is {} {}".format(number, get_last(), comval()))
