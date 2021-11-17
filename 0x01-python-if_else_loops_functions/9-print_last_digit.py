477f4ffc481aa6cdcaadf04ee59ae3d9dfd886cb
f-i-t-s-u-m <fibabirr@gmail.com>
2021-11-17 23:17:06 +0300

    update 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last = number % 10
-    if number < 0:
-        last = 10 - last
-    return last
+    if number:
+        last = number % 10
+        if number < 0:
+            last = 10 - last
+        return last
