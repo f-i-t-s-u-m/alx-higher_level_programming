d1ea9989a8d3d2e9f825a3ddf23bff1c49dde132
f-i-t-s-u-m <fibabirr@gmail.com>
2021-11-17 11:54:35 +0300

    update .

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for n in range (0, 100):
-    print("0{}, ".format(n)  if n < 10 else "{}, ".format(n) if n < 99 else "{}".format(n), end='')
+for n in range(0, 100):
+    print("{:0>2}, ".format(n) if n < 99 else "{}".format(n), end='')
 print()
