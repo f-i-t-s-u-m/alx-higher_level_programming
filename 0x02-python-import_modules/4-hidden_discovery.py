0fd49cc14e3b56fa13d6775638d14e4d51b2ef81
f-i-t-s-u-m <fibabirr@gmail.com>
2021-11-21 14:23:26 +0300

    update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
-if __name__ == '__main__'
+if __name__ == '__main__':
     for n in range(len(dir(hidden_4))):
         if dir(hidden_4)[n][0:1] != '_':
             print(dir(hidden_4)[n])
