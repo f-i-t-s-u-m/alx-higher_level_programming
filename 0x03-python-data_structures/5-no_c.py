36afdedd4106a07a3d76210ecf78b600248c5e46
f-i-t-s-u-m <fibabirr@gmail.com>
2021-11-22 21:27:59 +0300

    update 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if my_string:
+        new_string = ''
+        for n in my_string:
+            if n not in 'Cc':
+                new_string += n
+        return new_string
