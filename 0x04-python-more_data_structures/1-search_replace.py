771f9f0f464e40039b9ff9ad893c9f030e9bf83b
f-i-t-s-u-m <fibabirr@gmail.com>
2021-11-30 12:13:53 +0300

    update 1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = list()
+    if my_list:
+        new_list = list(map(lambda n: replace if n == search else n, my_list))
+    return new_list
