148aa7b8eceeb1861553305263e05a9310b139ff
f-i-t-s-u-m <fibabirr@gmail.com>
2021-12-01 08:38:37 +0300

    update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    g = sorted(my_list)
     sum1 = 0
-    for n in range(len(g)):
-        if g[n] != g[n - 1]:
-            sum1 += g[n]
+    for n in set(my_list):
+            sum1 += n
     return sum1
