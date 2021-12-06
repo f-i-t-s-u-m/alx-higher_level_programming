affea348d953e3a00a8f95f908ffac191b561793
f-i-t-s-u-m <fibabirr@gmail.com>
2021-12-07 00:08:02 +0300

    update 2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    n = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            n += 1
+    except IndexError:
+        print("", end="")
+    print("")
+    return n
