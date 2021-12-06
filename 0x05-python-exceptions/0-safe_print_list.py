141d49a63ac0086f1727e437bb134df79e897391
f-i-t-s-u-m <fibabirr@gmail.com>
2021-12-06 19:23:42 +0300

    update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,5 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[n]), end="")
     except IndexError:
         print("", end="")
-    finally:
-        print()
+    print()
     return x
