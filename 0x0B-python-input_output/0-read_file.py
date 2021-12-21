86b28270c62a5e71d9f355f92517e72ba57839cb
f-i-t-s-u-m <fibabirr@gmail.com>
2021-12-22 00:09:53 +0300

    update .

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,8 @@
 
 
 def read_file(filename=""):
-    with open(filename, "r") as f:
+    ''' python class to read file '''
+
+    with open(filename, "r", encoding="UTF-8") as f:
         for line in f:
             print(line, end="")
