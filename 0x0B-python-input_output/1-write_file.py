a1d5e377d0e146120accf315ad29917c934ef61e
f-i-t-s-u-m <fibabirr@gmail.com>
2021-12-22 00:43:21 +0300

    update .

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' python file '''
+
+
+def write_file(filename="", text=""):
+    ''' write to file'''
+
+    size = 0
+    with open(filename, "w", encoding="UTF-8") as f:
+        print(text, file=f, end="")
+    return len(text)
