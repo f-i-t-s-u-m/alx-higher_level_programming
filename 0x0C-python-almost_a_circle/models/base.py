aa7724a9d0aa437851668eb79869959708ab1afb
f-i-t-s-u-m <fibabirr@gmail.com>
2022-01-03 09:54:01 +0300

    update .

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''' almost circule '''
+
+
+class Base: 
+    ''' class of base '''
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        Base.__nb_objects += 1
+        self.id = id
+    
+    @property
+    def id(self):
+        return self.__id;
+
+    @id.setter
+    def id(self, value):
+        if value is not None:
+            self.__id = value
+        else:
+            self.__id = self.__nb_objects
+
