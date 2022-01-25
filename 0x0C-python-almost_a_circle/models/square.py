482a4e7015ba5ed9b0260c46e64722b0ef3576ed
fitsum belay <fibabirr@gmail.com>
2022-01-25 15:32:37 -0500

    square 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,6 +11,17 @@ class Square(Rectangle):
         """ init square class"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        "return size"
+        return self.width
+
+    @size.setter
+    def size(self,value):
+        """ size setter"""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """ return square string """
         return str("[Square] ({}) {}/{} - {}".format(
