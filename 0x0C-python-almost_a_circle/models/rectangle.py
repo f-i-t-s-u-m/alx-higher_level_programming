6d5830535ec1738883d5a64dbefe40ae9c183201
ozo <ozo@kali.fiba.net>
2022-01-25 13:47:51 -0500

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,6 +9,7 @@ class Rectangle(Base):
     ''' class of rectangle'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ init the class"""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -17,22 +18,27 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """ width property """
         return self.__width
 
     @property
     def height(self):
+        """ height getter"""
         return self.__height
 
     @property
     def x(self):
+        """ x getter """
         return self.__x
 
     @property
     def y(self):
+        """ y getter"""
         return self.__y
 
     @width.setter
     def width(self, value):
+        """ width setter """
         if not(isinstance(value, int)):
             raise TypeError("width must be an integer")
         elif value <= 0:
@@ -41,6 +47,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        """ height setter"""
         if not(isinstance(value, int)):
             raise TypeError("height must be an integer")
         elif value <= 0:
@@ -49,6 +56,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        """x settter """
         if not(isinstance(value, int)):
             raise TypeError("x must be an integer")
         elif value < 0:
@@ -57,6 +65,7 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        """ y setter"""
         if not(isinstance(value, int)):
             raise TypeError("y must be an integer")
         elif value < 0:
@@ -64,9 +73,11 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """return area"""
         return self.__width * self.__height
 
     def update(self, *args):
+        """ update area"""
         super().__init__(args[0])
         for i, val in enumerate(args[1:]):
             if i == 0:
@@ -79,6 +90,7 @@ class Rectangle(Base):
                 self.y = val
 
     def display(self):
+        """ display area"""
         for _ in range(self.y):
             print(" ")
         for _ in range(self.__height):
