962dad325d9f87321285008d3d09c4c15aa77129
fitsum belay <fibabirr@gmail.com>
2022-01-26 06:24:08 -0500

    update 13

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -38,6 +38,9 @@ class Square(Rectangle):
                 elif i == 2:
                     self.y = val
 
+    def to_dictionary(self):
+        return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
+
     def __str__(self):
         """ return square string """
         return str("[Square] ({}) {}/{} - {}".format(
