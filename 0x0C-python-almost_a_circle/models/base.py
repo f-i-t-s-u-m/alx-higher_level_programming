a7de5a03f595aaf3eb2bf935d6c245fb0c0085e7
fitsum belay <fibabirr@gmail.com>
2022-01-27 04:38:10 -0500

    update for task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,6 +18,15 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """static method to json string"""
+        a = list()
         if list_dictionaries is None or list_dictionaries is []:
             return []
-        return json.dumps(list_dictionaries)
+        for dics in list_dictionaries:
+            a.append(dics)
+        return json.dumps(a)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ save to file class methpd"""
+        with open(cls.__name__+'.json', "w") as jsfile:
+            json.dump(cls.to_json_string(list_objs[0:].to_dictionary()), jsfile)
