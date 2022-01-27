89e8d13453a56f35b63244e27e61ebe108ca858d
fitsum belay <fibabirr@gmail.com>
2022-01-27 03:23:09 -0500

    update for task 14

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,6 +18,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """static method to json string"""
-        if list_dictionaries is None:
-            return []
-        return json.dumps(list_dictionaries)
+        if list_dictionaries:
+            return json.dumps(list_dictionaries)
+        return []
