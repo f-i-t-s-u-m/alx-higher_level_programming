cf1ce01103da3ac30fcdb6368acade87085be547
fitsum belay <fibabirr@gmail.com>
2022-02-28 14:50:55 -0500

    fix to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const argv = process.argv.slice(2);
 if (argv === undefined || isNaN(parseInt(argv[0]))) console.log('Not a number');
-else console.log(parseInt(argv[0]));
+else console.log('My number: ' + parseInt(argv[0]));
