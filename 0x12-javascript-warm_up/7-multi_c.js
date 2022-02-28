2a63b5791164c4d126e52a72a87d457401550850
fitsum belay <fibabirr@gmail.com>
2022-02-28 15:19:55 -0500

    7 multi_c

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argv = process.argv.slice(2);
+if (argv[0] === undefined) console.log('Missing number of occurrences');
+else {
+	let n = argv[0]
+	while(n) {
+		console.log('C is fun');
+		n--;
+	}
+}
