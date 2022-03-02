bc6de3ed82b56a64c2f186a83edef97aef0ee63b
fitsum belay <fibabirr@gmail.com>
2022-03-02 12:57:02 -0500

    add 6-sq

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const Square = require('./5-square');
-module.exports = class Square extends Square {
+module.exports = class Square1 extends Square {
   charPrint (a = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(a.repeat(this.width));
