f69dec2666405608123eef30d44f3e707c79e931
fitsum belay <fibabirr@gmail.com>
2022-03-02 02:14:08 -0500

    fix 10-fa

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,8 @@
 const argv = process.argv.slice(2);
 let n = parseInt(argv[0]);
 function f (a) {
-  if (isNaN(n)) console.log(1);
+  if (argv.length >= 1 && isNaN(n));
+  else if (isNaN(n)) console.log(1);
   else if (a === 1) console.log(n);
   else {
     n = n * (a - 1);
