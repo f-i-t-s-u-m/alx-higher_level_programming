3ef8d25c4c598662b8f7d3c821ef7d31a1048114
fitsum belay <fibabirr@gmail.com>
2022-03-02 17:33:37 -0500

    add 10-facto

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
 const argv = process.argv.slice(2);
-let n = parseInt(argv[0]);
-function f (a) {
-  if (argv.length >= 1 && isNaN(n));
-  else if (isNaN(n)) console.log(1);
-  else if (a === 1) console.log(n);
-  else {
-    n = n * (a - 1);
-    f(a - 1);
+function factorial (a) {
+  if (a <= 0 || isNaN(a)) {
+    return 1;
   }
+  return a * factorial(a - 1);
 }
-
-f(n);
+console.log(factorial(parseInt(argv[0])));
