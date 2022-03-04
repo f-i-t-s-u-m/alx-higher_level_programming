9408f38a2727990b9504abb776afa76597928f29
fitsum belay <fibabirr@gmail.com>
2022-03-04 08:34:12 -0500

    add 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if '__main__' == __name__:
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(user = argv[1], password=argv[2], database=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for data in rows:
+        print(data)
