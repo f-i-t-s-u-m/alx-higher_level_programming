b4bc29ad5d91bf25ec5993bb8122448819a191fa
fitsum belay <fibabirr@gmail.com>
2022-03-04 09:04:06 -0500

    add my filter

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if '__main__' == __name__:
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(user = argv[1], password=argv[2], database=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}'  ORDER BY states.id ASC".format(argv[4]))
+    rows = cur.fetchall()
+    for data in rows:
+        print(data)
