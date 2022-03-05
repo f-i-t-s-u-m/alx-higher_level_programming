d02ad9d53aa3ddfba0ba71aa0dcf680e5eae5b7e
fitsum belay <fibabirr@gmail.com>
2022-03-05 15:08:30 -0500

    fix  model state

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,12 +4,13 @@ fetch state using sqlalchemy
 """
 
 
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sys import argv
-from sqlalchemy.orm import sessionmaker
-import pymysql
 if __name__ == '__main__':
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sys import argv
+    from sqlalchemy.orm import sessionmaker
+    import pymysql
+
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
         argv[1], argv[2], argv[3]))
     Session = sessionmaker(bind=engine)
