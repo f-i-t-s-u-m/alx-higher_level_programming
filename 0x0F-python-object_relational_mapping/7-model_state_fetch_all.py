784b788fd36b7bf7bdc97272ed282369b467ad47
fitsum belay <fibabirr@gmail.com>
2022-03-05 15:02:13 -0500

    add model state

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+import pymysql
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for i in session.query(State):
+        print("{}: {}".format(i.id, i.name))
