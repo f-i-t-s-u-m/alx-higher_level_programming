23847d524307c6eccc265118c320d4de54b7dcc9
fitsum belay <fibabirr@gmail.com>
2022-03-06 09:33:36 -0500

    fix 2-my*

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,8 +9,8 @@ if '__main__' == __name__:
     from sys import argv
     db = MySQLdb.connect(user=argv[1], password=argv[2], database=argv[3])
     cur = db.cursor()
-    q = "SELECT * FROM states WHERE name = '{}'  ORDER BY states.id ASC"
-    cur.execute(q.format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC"
+                .format(argv[4]))
     rows = cur.fetchall()
     for data in rows:
         print(data)
