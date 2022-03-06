4feaf1046c86ff0209ef8f0a9b93ec87d0602fc8
fitsum belay <fibabirr@gmail.com>
2022-03-06 08:00:44 -0500

    fix city model and 14-model

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -8,9 +8,11 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
+
 class City(Base):
+    ' city class '
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key = True)
-    name = Column(String(128), nullable = False)
-    state_id = Column(Integer, nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False)
