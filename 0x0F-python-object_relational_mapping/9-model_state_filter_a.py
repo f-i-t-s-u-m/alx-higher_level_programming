0a65f6948b182ef0e62e0ffdaa2b077e3c49972f
fitsum belay <fibabirr@gmail.com>
2022-03-06 01:58:12 -0500

    add 9-model

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+List all state containing letter a
+"""
+
+if __name__ == '__main__':
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+    from model_state import Base, State
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+            argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for i in session.query(State).filter(State.name.like('%a%')):
+        print("{}: {}".format(i.id, i.name))
