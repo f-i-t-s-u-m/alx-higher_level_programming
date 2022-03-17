dbe5f7b30d1b65f5a280b6eb28fd74f141d587fc
fitsum belay <fibabirr@gmail.com>
2022-03-17 17:03:47 -0400

    fix 1

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#get status code
-curl  -sI -o /dev/null -w "%{http_code}" "$1"
-
+# sends a request to a URL passed as an argument, and displays only the status code of the response.
+curl -sI -o /dev/null -w "%{http_code}" "$1"
