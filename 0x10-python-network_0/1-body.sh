121538e8c65f86ae71d63eb178a97e20091023b3
fitsum belay <fibabirr@gmail.com>
2022-03-17 15:09:49 -0400

    fix 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #get body if status code is 200
 
-(($#)) && RES=$(curl  -s -I $1 | grep HTTP |  awk '{print $2}')
-(($RES == 200)) && curl -s $1
+(($#)) && RES=$(curl  -s -I $1 | grep HTTP |  awk '{print $2}') && (($RES == 200)) && curl -s $1
