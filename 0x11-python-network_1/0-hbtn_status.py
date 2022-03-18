38edcf78f0909ecb90310aed5b48449647543859
fitsum belay <fibabirr@gmail.com>
2022-03-18 05:16:13 -0400

    init 0x11 with readme and first task

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from urllib import request
+
+""" get status code """
+
+
+with request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+    data = resp.read()
+
+print("Body response: ")
+print("    - type: {}".format(type(data)))
+print("    - content: {}".format(data))
+print("    - utf8: {}".format(data.decode('UTF8')))
