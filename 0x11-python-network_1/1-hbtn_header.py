a203bf9b8baca6e5c899255a624b3382ae87e417
fitsum belay <fibabirr@gmail.com>
2022-03-18 06:18:35 -0400

    fix one more times

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,8 +3,8 @@ from urllib import request
 from sys import argv
 """ get x data """
 
-
-with request.urlopen(argv[1]) as resp:
-    for i in resp.getheaders():
-        if i[0] == 'X-Request-Id':
-            print(i[1])
+if __name__ == '__main__':
+    with request.urlopen(argv[1]) as resp:
+        for i in resp.getheaders():
+            if i[0] == 'X-Request-Id':
+                print(i[1])
