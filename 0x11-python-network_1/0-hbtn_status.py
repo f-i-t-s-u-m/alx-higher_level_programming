f1a507f969c37fad9e249cafc90868c31ca2fb8b
fitsum belay <fibabirr@gmail.com>
2022-03-18 06:05:55 -0400

    fix one

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ from urllib import request
 with request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
     data = resp.read()
 
-print("Body response: ")
+print("Body response:")
 print("    - type: {}".format(type(data)))
 print("    - content: {}".format(data))
 print("    - utf8: {}".format(data.decode('UTF8')))
