6b4fd0ecde11de9d5c31bf65d1c7f46927d5e9ba
fitsum belay <fibabirr@gmail.com>
2022-03-18 14:52:50 -0400

    fix task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """ check status """
+import requests
 
 if __name__ == "__main__":
-    from urllib import request
-
-    with request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
-        data = resp.read().decode('UTF8')
-
+    data = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print('\t- type: {}'.format(type(data)))
-    print('\t- content: {}'.format(data))
+    print('\t- type: {}'.format(type(data.text)))
+    print('\t- content: {}'.format(data.text))
