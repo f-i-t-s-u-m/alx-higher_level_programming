64497ffdfa786b690ad0f48a0b9679765b5d4cd1
fitsum belay <fibabirr@gmail.com>
2022-03-18 15:16:13 -0400

    fix task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" response header value """
+
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    res = requests.post(argv[1], {'email': argv[2]})
+    print(res.text)
