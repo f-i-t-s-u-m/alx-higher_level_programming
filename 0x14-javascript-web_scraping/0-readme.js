f7ca3260c94ea3726b31b44feb40d88fb8ab9777
f-i-t-s-u-m <fibabirr@gmail.com>
2022-04-12 15:16:54 +0300

    fix semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-var argv = process.argv.slice(2);
-var fs =  require('fs');
+const argv = process.argv.slice(2);
+const fs = require('fs');
 
 fs.readFile(argv[0], 'utf8', function (err, data) {
   if (err) {
-    return console.log(err)
+    return console.log(err);
   }
-  console.log(data)
-})
+  console.log(data);
+});
