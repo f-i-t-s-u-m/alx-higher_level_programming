566b9e6abea63ee4604b2ad5b54ef45de23ca3df
f-i-t-s-u-m <fibabirr@gmail.com>
2022-04-12 17:47:04 +0300

    add 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@
 const req = require('request');
 const argv = process.argv.slice(2);
 
-req(argv[0], (error, response, body) => {
+req('https://swapi-api.hbtn.io/api/films/' + argv[0], (error, response, body) => {
   if (error) {
     console.log(error);
   }
