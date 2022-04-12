e662bdf1e41e01bd7ba44d3862e4719da92879f9
f-i-t-s-u-m <fibabirr@gmail.com>
2022-04-13 00:17:10 +0300

    fix 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,15 +8,8 @@ req(argv[0], (error, response, body) => {
     console.log(error);
   }
 
-  let i = 0;
-  let c;
-  while (JSON.parse(body).results[i]) {
-    let n = 0;
-    const ch = JSON.parse(body).results[i].characters;
-      let c ch.filter('https://swapi-api.hbtn.io/api/people/18/')
-      n++;
-    }
-    i++;
-  }
-  console.log(c);
+    const ch = JSON.parse(body).results.filter(result => {
+	return result.characters.filter(data => data.includes('18')).length;
+    }).length;
+  console.log(ch)
 });
